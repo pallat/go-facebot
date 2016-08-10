5a8a1130207807510bc53b7c3793ac6b8e1b9b41
pallat <pallat@dtac.co.th>
2016-08-10 15:42:35 +0700

    log step

--- event_handler.go
+++ event_handler.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"net/http"
 )
 
 func receivedAuthentication(msg Messaging) error {
+	fmt.Println("receivedAuthentication....")
 	return sendTextMessage("", msg.Sender.ID, "", "Authentication successful")
 }
 func receivedMessage(pageID string, msg Messaging) error {
+	fmt.Println("receivedMessage....")
 	if msg.Message.Text == "" {
 		return nil
 	}
+	fmt.Println("receivedMessage....", msg.Message.Text)
 
 	switch msg.Message.Text {
 	// case "image":
@@ -20,6 +24,7 @@ func receivedMessage(pageID string, msg Messaging) error {
 	// case "receipt":
 	// case "555":
 	default:
+		fmt.Println("sendTextMessage....")
 		return sendTextMessage(pageID, msg.Sender.ID, msg.Message.MID, msg.Message.Text)
 	}
 
@@ -37,6 +42,7 @@ func sendTextMessage(pageID, id, mid, text string) error {
 			"text": "` + text + `"
 		}
 	}`
+	fmt.Println("sendTextMessage....", messageData)
 
 	// "sender":{
 	//   "id":"` + pageID + `"
@@ -52,12 +58,15 @@ func callSendAPI(data string) error {
 			DisableKeepAlives: true,
 		},
 	}
+	fmt.Println("callSendAPI....")
 
 	r, err := http.NewRequest("POST", "https://graph.facebook.com/v2.6/me/messages?access_token="+pageAccessToken, bytes.NewBufferString(data))
 	if err != nil {
 		return err
 	}
 
+	fmt.Println("callSendAPI....DO")
 	_, err = c.Do(r)
+	fmt.Println("callSendAPI....DONE")
 	return err
 }
